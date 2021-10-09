f1e0415161ea39fbb28d9e0c7cc1b8b160350676
Ana cristina da silva Vieira <anac.s.vieira@hotmail.com>
2021-10-09 10:25:19 -0400

    Corrigindo o codigo rust

--- questao2.rs
+++ questao2.rs
@@ -1,13 +1,27 @@
 fn main(){
 
    
-    let x;
-
-    let a=1;
-    let b=1;
+    let mut x:i32 =0;
+    let mut a:i32 =1;
+    let mut b:i32 =1;
     
     x = a & b;
-    println!("{:0b} and {:0b} = {:0b}", a,b,!x)
+    println!("{:0b} and {:0b} = {:0b}", a, b,!x);
+    
+    a = 1;
+    b = 0;
+    x = a & b;
+    println!("{:0b} and {:0b} = {:0b}", a, b,!x);
+    
+    a=0;
+    b=1;
+    x = a & b;
+    println!("{:0b} and {:0b} = {:0b}", a, b,!x);
+    
+     a=0;
+     b=0;
+    x = a & b;
+    println!("{:0b} and {:0b} = {:0b}", a, b,!x);
 
 
 }
